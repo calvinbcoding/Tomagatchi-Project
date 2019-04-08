6752988145660b42d9bfee0ec8dd71159a812ac5
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-08 10:32:15 -0600

    "working timer, with death alerts"

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 
 
-let timePassing(){
-  while()
-};
+
 let seconds = 0;
 
 
 
 
 class Tomagotchi {
-    constructor(name) {
-      this.name = name;
+    constructor() {
+      this.name = "name";
       this.age = 0;
       this.sleepiness = 0;
       this.boredom = 0;
@@ -20,15 +18,16 @@ class Tomagotchi {
 
 let myPet = new Tomagotchi(name);
 
-function myPetDied() {
-  console.warn('seconds', seconds)
-    if(myPet.boredom >= 10)
-      console.log(myPet.name + " was bored to death.");
-    if(myPet.sleepiness >= 10)
+
+    if(myPet.boredom >= 10){
+      alert(myPet.name + " was bored to death.");
+    }
+    if(myPet.sleepiness >= 10){
       console.log(myPet.name + " went on a crack binge and died of sleep deprivation.");
-    if(myPet.hunger >= 10)
+    }
+    if(myPet.hunger >= 10){
       console.log(myPet.name + " has starved to death.");
-    };
+    }
     
  
 
@@ -38,7 +37,8 @@ const secondsGoUp = () => {
 
         if(seconds % 9 === 0)
           {myPet.age++;
-            $('.age').text('Age: ' + myPet.age)}
+            $('.age').text('Age: ' + myPet.age)
+            console.log(myPet.name + " is" + myPet.age + "years old")}
 
         if(seconds % 7 ===0)
           {myPet.boredom++;
@@ -51,27 +51,52 @@ const secondsGoUp = () => {
         if(seconds % 11 === 0)
         {myPet.sleepiness++;
           $('.sleepiness').text('Sleepiness: ' + myPet.sleepiness)}
-        }
 
-$('.submit').click(function() {
+
+    if(myPet.boredom >= 10){
+      console.log(myPet.name + " was bored to death.");
+    }
+    if(myPet.sleepiness >= 10){
+      console.log(myPet.name + " went on a crack binge and died of sleep deprivation.");
+    }
+    if(myPet.hunger >= 10){
+      console.log(myPet.name + " has starved to death.");
+    }
+    
+        };
+
+$('.submit').on('click', () => {
     $('.age').text(seconds);
-      timePassing = setInterval(secondsGoUp, 1200);
+      window.timePassing = setInterval(secondsGoUp, 1000)});
 
-$('.name').val('myPet.name');
-        $('#pet-name').text('Name: ' + myPet.name);
-      zz
+
+    // $('#pet-name').text('Name: ' + myPet.name);
+      
     
 $('.attention').on('click', () => {
     myPet.boredom --
-})
+  });
+  // let $it;
+  //    $(document).on('click', (e) => {
+  //      $it = $(e.target);
+  //    })
+
 $('.sleep').on('click', () => {
     myPet.sleepiness -- 
-})
+});
 $('.food').on('click', () => {
     myPet.hunger -- 
-})
-$('.rick').fadeToggle('click')
+});
+// $('.Rick').fadeToggle('keypress', () => {
 
-$('.startover').on('click', () => {
-        clearInterval(timepassing)
-      });
+// });
+
+// $('#updatePetBtns').on('click', myPet);
+// myPetDied();
+
+const stopInterval = () => {
+  clearInterval(window.timePassing)
+}
+
+$('.startover').on('click',function(){stopInterval();}
+      );
